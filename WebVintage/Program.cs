d9bcc23800451fba3a06d50c028a7ebb3a7413fa
novoluisina <novoluisina@gmail.com>
2024-09-12 20:09:41 -0300

    xx

--- WebVintage/Program.cs
+++ WebVintage/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure.ApplicationDbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<VintageDbContext>(options =>
+options.UseMySql(builder.Configuration.GetConnectionString("connection"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.15-mysql")));
+
 
 var app = builder.Build();
 
